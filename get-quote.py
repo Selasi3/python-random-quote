57cd325535ad5afffd173c6d9189e762dca2235a
Selasi3 <selasiayittah3@gmail.com>
2021-10-10 04:31:04 -1200

    New changes

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,29 @@
 def primary():
- #print("Keep it logically awesome.")
+  f = open("quotes.txt","a")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
+ #Adding new quotes to the quotes.txt
+  add_quotes = ["Two heads are better than one",
+                "One step at a time",
+                "Time waits for no man"
+                ] 
+  for i in add_quotes:
+    f.writelines(i +"\n")
   f.close()
 
+  f = open("quotes.txt", "r")
+  quotes = f.readlines()
+  #Read the last line 
+  print("Last quote")
   print(quotes[-1])
+  #Reading the first five lines
+  print("First five lines")
+  for i in range(5):
+
+    print(i+1,quotes[i], sep=" ")
+
+
+  #print the last line of a file
+  #print(quotes[-1])
 
 if __name__== "__main__":
   primary()
